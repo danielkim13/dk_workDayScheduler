c40128c4e6c0b277d2f5a7dc27a7c926e640695a
Daniel Kim <daniel.d.kim13@gmail.com>
2021-12-25 13:18:24 -0500

    saving what I have so far. saveSchedule function needs some work. JSON is not being read

--- assets/js/workDay.js
+++ assets/js/workDay.js
@@ -7,3 +7,25 @@ currentDay();
 setInterval(function () {
   currentDay();
 }, 1000 * 60); //testing this I realized that the a minute clock starts when user opens the page so it doesn't synch with the computer time. but for now this will do.
+
+// init localStorage and saving textarea input function function.
+function saveSchedule() {
+  let workDaySchedule = JSON.parse(localStorage.getItem("hourlyTask")) || [];
+  const hour = $(".hour").val();
+  console.log(hour);
+  const toDo = $("#text").val();
+  console.log(toDo);
+  const task = {
+    hour: hour,
+    schedule: toDo,
+  };
+
+  workDaySchedule.push(task);
+  localStorage.setItem("hourlyTask", JSON.stringify(workDaySchedule));
+}
+
+// event listener for the button to save the task.
+$(".saveBtn").on("click", saveSchedule);
+// initial display of local storage if there is data in the localStorage.
+
+// make p box into textarea when clicked and able to edit.. once done, you can save and the element returns back to p element.
