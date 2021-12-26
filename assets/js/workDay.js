b82444e8187daa214d33ad2cae47eecdf7daff28
Daniel Kim <daniel.d.kim13@gmail.com>
2021-12-26 11:28:43 -0500

    saving the hour and comment to localStorage

--- assets/js/workDay.js
+++ assets/js/workDay.js
@@ -8,24 +8,28 @@ setInterval(function () {
   currentDay();
 }, 1000 * 60); //testing this I realized that the a minute clock starts when user opens the page so it doesn't synch with the computer time. but for now this will do.
 
-// init localStorage and saving textarea input function function.
+// saving the hour and comment to local storage (tutor help needed to make the hour and comment into object and push it into local storage and getit from local storage.)
 function saveSchedule() {
-  let workDaySchedule = JSON.parse(localStorage.getItem("hourlyTask")) || [];
-  const hour = $(".hour").val();
+  // selecting time-block div id.. since it is unique to the hours i need to do nearby value.
+  const hour = $(this).parent().attr("id"); // tried .closest("id").val(): undefined.. .parent().val("id"): displayed data that I wasn't looking for.
   console.log(hour);
-  const toDo = $("#text").val();
-  console.log(toDo);
-  const task = {
-    hour: hour,
-    schedule: toDo,
-  };
+  const comment = $(this).siblings("#text").val(); // tried .closest('#text"): not what i'm looking for. jQuery doc states closest begins w/ current element and travels up the DOM tree.. meaning it will go to parent node not sibling.
+  console.log(comment);
 
-  workDaySchedule.push(task);
-  localStorage.setItem("hourlyTask", JSON.stringify(workDaySchedule));
+  // pushing them into localStorage. since both hour and comment are string, I can make hour as key and comment as value.
+  localStorage.setItem(hour, comment);
 }
 
 // event listener for the button to save the task.
 $(".saveBtn").on("click", saveSchedule);
 // initial display of local storage if there is data in the localStorage.
-
-// make p box into textarea when clicked and able to edit.. once done, you can save and the element returns back to p element.
+// each key needs to be pulled into display. jQuery $ selector of parent id child id val.. get the value from the storage?
+$("#hour_9 #text").val(localStorage.getItem("hour_9"));
+$("#hour_10 #text").val(localStorage.getItem("hour_10"));
+$("#hour_11 #text").val(localStorage.getItem("hour_11"));
+$("#hour_12 #text").val(localStorage.getItem("hour_12"));
+$("#hour_13 #text").val(localStorage.getItem("hour_13"));
+$("#hour_14 #text").val(localStorage.getItem("hour_14"));
+$("#hour_15 #text").val(localStorage.getItem("hour_15"));
+$("#hour_16 #text").val(localStorage.getItem("hour_16"));
+$("#hour_17 #text").val(localStorage.getItem("hour_17"));
